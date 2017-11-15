a91adb9d6edfe80fa1adab6f9a6dc8c3ad527be0
mariko suga <m.suga@ohako-inc.jp>
2017-11-15 19:30:24 +0900

    add gulp task for sever

---
+++ gulp/tasks/server.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const WebpackDevServer = require('webpack-dev-server');
+const webpack = require('webpack');
+const webpackConfig = require('../../webpack.config');
+const paths = require('../paths');
+
+gulp.task('server', () => {
+  let myConfig = Object.create(webpackConfig);
+  myConfig.devtool = 'eval';
+  // myConfig.debug = true;
+
+  new WebpackDevServer(webpack(myConfig), {
+    publicPath: `/${myConfig.devServer.contentBase}`,
+    stats: {
+      colors: true
+    },
+    contentBase:`${myConfig.devServer.contentBase}`
+  }).listen(myConfig.devServer.port, 'localhost', function(err) {
+    if(err) throw new gutil.PluginError('webpack-dev-server', err);
+    gutil.log('[webpack-dev-server]', `http://localhost:${myConfig.devServer.port}/webpack-dev-server/index.html`);
+  });
+});
