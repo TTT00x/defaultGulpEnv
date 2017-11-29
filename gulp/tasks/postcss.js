3ec735a0d58245c13dd5d7ec2113b162f5a45b32
mariko suga <m.suga@ohako-inc.jp>
2017-11-29 14:01:49 +0900

    add gulp task for css

---
+++ gulp/tasks/postcss.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
+const cssimport = require('postcss-import');
+const simplevars = require('postcss-simple-vars');
+const paths = require('../paths');
+
+gulp.task('css', function() {
+  const plugins = [
+    cssimport,
+    simplevars,
+    nested,
+    autoprefixer,
+  ];
+  return gulp.src(`${paths.SRC}postcss/style.css`)
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(`${paths.DEV}css`))
+});
