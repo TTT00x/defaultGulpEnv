04090a960ee029a366d3510f5f50543c5801cfa8
mariko suga <m.suga@ohako-inc.jp>
2017-11-29 15:12:15 +0900

    fix gulp task for css

--- gulp/tasks/postcss.js
+++ gulp/tasks/postcss.js
@@ -6,7 +6,7 @@ const cssimport = require('postcss-import');
 const simplevars = require('postcss-simple-vars');
 const paths = require('../paths');
 
-gulp.task('css', function() {
+gulp.task('css', () => {
   const plugins = [
     cssimport,
     simplevars,
