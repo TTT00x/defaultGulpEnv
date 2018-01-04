febc17e4e6251078b4447e2bec01be15c303c164
mariko suga <m.suga@ohako-inc.jp>
2018-01-04 12:02:49 +0900

    update image task

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp');
+const del = require('del');
 const imagemin = require('gulp-imagemin');
 const paths = require('../paths');
 
-gulp.task('image', () => {
+gulp.task('delImage', () => {
+  return del([
+    `${paths.DEV}img/**`,
+    `!${paths.DEV}img/sprite/**`,
+  ])
+});
+
+gulp.task('image', ['delImage'], () => {
   const options = {optimizationLevel: 7};
 
-  return gulp.src([`${paths.SRC}img/**`, `!${paths.SRC}img/sprite/`])
+  return gulp.src([
+    `${paths.SRC}img/*.+(jpg|jpeg|png|gif|svg)`,
+    `${paths.SRC}img/**/*.+(jpg|jpeg|png|gif|svg)`,
+    `!${paths.SRC}img/sprite/**`
+  ])
     .pipe(imagemin(options))
     .pipe(gulp.dest(`${paths.DEV}img`));
 });
