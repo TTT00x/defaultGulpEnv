96aa5d54a09a8c01e6639a4881977e9195503767
mariko suga <m.suga@ohako-inc.jp>
2018-03-13 11:50:50 +0900

    update StopScroll module

--- src/js/module/StopScroll.js
+++ src/js/module/StopScroll.js
@@ -3,13 +3,16 @@ class StopScroll {
     this.init();
   }
   init() {
+    this.window = window;
     this.body = document.body;
   }
   stop() {
-    this.body.style.cssText = 'overflow: hidden;';
+    this.mount = this.window.pageYOffset;
+    this.body.style.cssText = `overflow: hidden; position: fixed; top: -${this.mount}px; left: 0;`;
   }
   reStart() {
-    this.body.style.cssText = 'overflow: auto;';
+    this.body.style.cssText = 'overflow: auto; position: static;';
+    this.window.scrollTo(0, this.mount);
   }
 }
 export default StopScroll;
