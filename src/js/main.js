1af1b7b34b3579de2c717809fec008dc4904e631
mariko suga <m.suga@ohako-inc.jp>
2018-03-26 10:39:40 +0900

    eliminate lint error

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
 import svg4everybody from 'svg4everybody'; // svg表示のためのIE11用polyfill
-import ua from './util/ua';
-import convertToArray from './module/convertToArray';
-import StopScroll from './module/StopScroll';
 
 require('what-input'); // キーボード操作とマウス操作判定用ライブラリ
 
 window.onload = () => {
   svg4everybody();
-
 };
