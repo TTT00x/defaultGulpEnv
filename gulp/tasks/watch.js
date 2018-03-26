458bc261458f70b829c58796e287ffc5ba4f420f
mariko suga <m.suga@ohako-inc.jp>
2018-03-26 10:38:10 +0900

    put watch task in order

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,9 +2,8 @@ const gulp = require('gulp');
 const paths = require('../paths');
 
 gulp.task('watch', () => {
-  gulp.watch([`${paths.SRC}pug/*.pug`, `${paths.SRC}pug/**/*.pug`], ['pugReload']);
-  gulp.watch([`${paths.SRC}scss/*.scss`, `${paths.SRC}scss/**/*.scss`], ['sassReload']);
-  // gulp.watch([`${paths.SRC}postcss/*.css`, `${paths.SRC}postcss/**/*.css`], ['postcssReload']);
-  gulp.watch([`${paths.SRC}js/*.js`, `${paths.SRC}js/**/*.js`], ['jsReload']);
-  gulp.watch([`${paths.SRC}js/*.js`, `${paths.SRC}js/**/*.js`], ['lint']);
+  gulp.watch([`${paths.SRC}pug/**/*.pug`], ['pugReload']);
+  gulp.watch([`${paths.SRC}scss/**/*.scss`], ['sassReload']);
+  gulp.watch([`${paths.SRC}js/**/*.js`], ['jsReload']);
+  gulp.watch([`${paths.SRC}js/**/*.js`], ['lint']);
 });
